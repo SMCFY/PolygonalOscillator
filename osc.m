d36470a0fd407fe27a9dd1ff34c4a492c6ae893c
gericsapo <gericsapo@gmail.com>
2018-03-16 17:05:52 +0100

    wavetable implementation

--- osc.m
+++ osc.m
@@ -1,33 +1,73 @@
-
-fs = 44100;
-dur = 1;
-t = [0:fs-1]/fs; 
-
 % core parameters
-f = 200; % f0
+f = 120; % f0
 n = 4; % order (vertex count)
 T = 0; % teeth
 phaseOffset = pi/4; % initial phase
 
-theta = 2*pi*f*t; % phase angle [radians]
 
-% polygon 
-r = zeros(1, length(theta)); % polygon amplitude
-for i=1:length(r)
-r(i) = cos(pi/n) / cos(mod(theta(i), 2*pi/n) -pi/n + T);
+% wavetable
+tableSize = 256;
+waveTable = zeros(1, tableSize+1);
+
+t = [0:tableSize-1]/tableSize; % time vector
+theta = 2*pi*1*t; % phase angles of a period
+ 
+for i=1:tableSize % geometry
+    waveTable(i) = cos(pi/n) / cos(mod(theta(i), 2*pi/n) -pi/n + T);
 end
 
+poly = zeros(1, tableSize); % sampled geometry
+for i=1:tableSize
+    poly(i) = waveTable(i) * (cos(theta(i)+phaseOffset) + 1j*sin(theta(i)+phaseOffset));
+end
 
-% sampling and projection
-poly = r .* (cos(theta+phaseOffset) + 1j*sin(theta+phaseOffset)); % sampled polygon
-polyWav = imag(poly); % projection to y axis
+%poly = waveTable .* (cos(theta+phaseOffset) + 1j*sin(theta+phaseOffset)); % sampled polygon
+waveTable = imag(poly); % projection to y axis
+%waveTable = [waveTable, waveTable(1)]; % wrap waveform around
 
 %% plot
 subplot(2,1,1);
 plot(real(poly), imag(poly));
 
 subplot(2,1,2);
-plot(polyWav);
+plot(waveTable);
 
 %% sound
-soundsc(polyWav, fs);
+duration = 3; 
+fs = 44100;
+tableOverSamplingRate = tableSize / fs;
+tableDelta = f * tableOverSamplingRate; % read increment for wavetable
+readIndex = 1; % sample need to be read from wavetable
+
+y = zeros(1, fs*duration); % output
+
+
+for n=1:length(y) % synhtesis from wavetable
+    
+    % boundary indices and corresponding values
+    i1 = floor(readIndex+1); 
+    if i1 == tableSize
+        i2 = 1;
+    else
+        i2 = i1+1;
+    end
+    v1 = waveTable(i1);
+    v2 = waveTable(i2);
+  
+    frac = readIndex - i1; % sample fraction
+    
+    y(n) = v1 + (frac*(v2-v1)); %interpolation
+    
+    
+    readIndex = readIndex + tableDelta;
+    if readIndex > tableSize
+        readIndex = readIndex-tableSize;
+    end
+    
+end
+
+
+
+soundsc(y, fs);
+figure();
+plot(y);

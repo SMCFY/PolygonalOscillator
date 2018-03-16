a233d2f5607d905b1d754bf5a627dfefb80b07f9
gericsapo <gericsapo@gmail.com>
2018-03-16 12:26:30 +0100

    cleanup

--- osc.m
+++ osc.m
@@ -1,24 +1,28 @@
+
 fs = 44100;
 dur = 1;
-t = [0:1/fs:dur-(1/fs)];
+t = [0:fs-1]/fs; 
 
 % core parameters
-f = 80; theta = 2*pi*f*t; % phase angle
+f = 200; % f0
 n = 4; % order (vertex count)
 T = 0; % teeth
 phaseOffset = pi/4; % initial phase
 
-for i=1:length(theta)
-%r(i) = cos(pi/n) / cos(2*pi/n * mod(theta(i)*n/2*pi, 1) - pi/n + T); % polygon amplitude
-r(i) = cos(pi/n) ./ cos(mod(theta(i), 2*pi/n) -pi/n + T);
+theta = 2*pi*f*t; % phase angle [radians]
+
+% polygon 
+r = zeros(1, length(theta)); % polygon amplitude
+for i=1:length(r)
+r(i) = cos(pi/n) / cos(mod(theta(i), 2*pi/n) -pi/n + T);
 end
 
-%poly = ones(1,fs+1) .* (cos(theta+phaseOffset) + 1j*sin(theta+phaseOffset)); % sinewave
+
+% sampling and projection
 poly = r .* (cos(theta+phaseOffset) + 1j*sin(theta+phaseOffset)); % sampled polygon
 polyWav = imag(poly); % projection to y axis
 
 %% plot
-
 subplot(2,1,1);
 plot(real(poly), imag(poly));
 

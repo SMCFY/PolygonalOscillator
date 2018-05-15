95e865f6098075f84ac795d3768892efc5282e1c
gericsapo <gericsapo@gmail.com>
2018-05-15 14:38:49 +0200

    audio rate synthesis ...loosing my mind

--- oscAudio.m
+++ oscAudio.m
@@ -1,69 +1,68 @@
-%% wavetable
-
-tableSize = 256;
-%waveTable = zeros(1, tableSize);
+%% synthesis
 
 % core parameters
-N = 3; % schlalfi numerator
-np = 1; % schalfi denominator (number of periods)
-n = N/np; % order (schlalfi symbol) (n>2)
-f0 = 80/np; % f0
+a = 3; % schlalfi numerator
+b = 1; % schalfi denominator (a > 2b)
+n = a/b; % order (schlalfi symbol)
+f0 = 440; % f0
 T = 0.0; % teeth
-phaseOffset = pi / 2; % initial phase
+phaseOffset = 0; % initial phase
 R = 1; % scale
 
+fs = 44100;
+dPhase = 2*pi * (f0/fs); % phase increment
+sizeP = ceil(fs/f0); % period in samples
 
-t = [0:tableSize-1]/tableSize; % time vector
-theta = 2*pi*t*np; % phase angle
-p = zeros(1, tableSize); % radial amplitude of geometry
- 
-for i=1:tableSize % geometry
-    p(i) = cos(pi/n) / cos(mod(theta(i)+phaseOffset, 2*pi/n) -pi/n + T) * R; % (modified Eq1) 
+theta = phaseOffset; % init phase radial amplitude calculation
+p = zeros(1, fs); % radial amplitude of geometry
+for i=1:fs % geometry
+    p(i) = cos(pi/n) / cos(mod(theta, 2*pi/n) -pi/n + T) * R;
+    theta = theta+dPhase;
 end
 
-poly = zeros(1, tableSize); % sampled geometry
-for i=1:tableSize
-    poly(i) = p(i) * (cos(theta(i)) + 1j*sin(theta(i)));
+theta = 0; % init phase for sampling
+poly = zeros(1, fs); % sampled geometry
+for i=1:fs
+    poly(i) = p(i) * (cos(theta) + 1j*sin(theta));
+    theta = theta+dPhase;
 end
 
-waveTable = imag(poly); % projection to y axis
+waveform = imag(poly); % projection to y axis
 
 %% anti aliasing
 
-dx = diff(waveTable); % first derivative of the waveform
+dx = diff(waveform); % first derivative of the waveform
 dx = dx / max(abs(dx)); % normalisation
 dx = [dx dx(1)]; % wrap around
 
-waveTableAA = waveTable; % anti-aliased waveform
-disc = zeros(1, N); % location of discontinuities expressed in samples
+waveformAA = waveform; % anti-aliased waveform
+disc = zeros(1, a); % location of discontinuities expressed in samples
 
-for k=1:N % iterates through discontinuities
-    disc(k) = mod(tableSize/N*k+1 - tableSize/np/(2*pi / phaseOffset), tableSize);
+for k=1:a % iterate through the discontinuities in the first period
     
-    % boundary samples (wrapped around the wavetable)
-    n3 = mod(ceil(disc(k)), tableSize);
-    n1 = mod(n3-2+tableSize, tableSize);
-    n2 = n1+1; %mod(n3-1+tableSize, tableSize);
-    n4 = mod(n3+1+tableSize, tableSize);
-
-    d = mod(n3 - disc(k), tableSize); % fractional delay between the discontinuity and the next sample (Eq.5)
-
-    %u = abs(dx(n2)-dx(n3)); % change in amplitude at the discontinuities
-    %u = (-sin(2*pi/N*k)*cos(pi/N)+sin(pi/N)*cos(2*pi/N*k) / cos(pi/n)) - (-sin(2*pi/N*k)*cos(pi/N)-sin(pi/N)*cos(2*pi/N*k) / cos(pi/n));
-    u = abs(-2*tan(pi/N)*cos(2*pi/N*k)); % change in amplitude at the discontinuities (Eq.11)
+    disc(k) = fs/(n*f0)*k - fs/f0/(2*pi/phaseOffset) + 1;
+   
+    % boundary sample (wrapped around a single period)
+    n3 = ceil(disc(k));
+    n1 = n3-2;
+    n2 = n1+1;
+    n4 = n3+1;
     
-    % 4-point polyBLAMP residual coefficients (Table.1)
+    d = n3 - disc(k); % fractional delay between the discontinuity and the next sample 
+    
+    u = -2*tan(pi/n) * cos((2*pi/n)*k); % slope of the derivative at the discontinuity
+   
+    % 4-point polyBLAMP residual coefficients
     p0 = d^5/120;
     p1 = (-3*d^5 +5*d^4 +10*d^3 +10*d^2 +5*d +1)/120;
     p2 = (3*d^5 -10*d^4 +40*d^2 -60*d +28)/120;
     p3 = (-d^5 +5*d^4 -10*d^3 +10*d^2 -5*d +1)/120;
     
     % waveform correction on the four samples around the discontinuity
-    waveTableAA(n1) = waveTable(n1) -p0*u *sign(waveTable(n1));
-    waveTableAA(n2) = waveTable(n2) -p1*u *sign(waveTable(n2));
-    waveTableAA(n3) = waveTable(n3) -p2*u *sign(waveTable(n3)); 
-    waveTableAA(n4) = waveTable(n4) -p3*u *sign(waveTable(n4));
-    
+    waveformAA(n1) = waveform(n1) -p0*u *sign(waveform(n1));
+    waveformAA(n2) = waveform(n2) -p1*u *sign(waveform(n2));
+    waveformAA(n3) = waveform(n3) -p2*u *sign(waveform(n3)); 
+    waveformAA(n4) = waveform(n4) -p3*u *sign(waveform(n4));
 end
 
 %% plot
@@ -75,64 +74,32 @@ axis equal;
 title('Complex plane');
 
 subplot(2,1,2);
-graph1 = plot(waveTable, 'b');
-axis([0 tableSize -1 1]);
+graph1 = plot(waveform, 'b');
+axis([1 sizeP -1 1]);
 hold on;
 graph2 = plot(dx, '--');
 hold on;
-graph3 = plot(waveTableAA, '-.m');
-for i=1:N
+graph3 = plot(waveformAA, '-.m');
+for i=1:a
     line([disc(i),disc(i)], [-1,1],'Color','red','LineStyle',':');
 end
 legend([graph1, graph2, graph3],{'Waveform', 'Derivative', 'Anti-aliased waveform'});
 title('Projection');
-%% synthesis
 
-duration = 1; 
-fs = 44100;
-tableOverSamplingRatio = tableSize / fs;
-tableDelta = f0 * tableOverSamplingRatio; % read increment for wavetable
-readIndex = 1; % table read index
+%% output
 
-y = zeros(1, fs*duration); % output
+duration = 1;
 
+y = zeros(1, duration*fs);
+readIndex = 1;
 
-for i=1:length(y) % synhtesis from wavetable
+for i=1:length(y)
+    y(i) = waveform(readIndex);
     
-    % boundary indices and corresponding values
-    i1 = floor(readIndex+1); 
-    if i1 == tableSize
-        i2 = 1;
-    else
-        i2 = i1+1;
+    readIndex = mod(readIndex+1, sizeP);
+    if(readIndex == 0)
+        readIndex = 1;
     end
-    v1 = waveTable(i1);
-    v2 = waveTable(i2);
-  
-    frac = readIndex - i1; % sample fraction
-    
-    y(i) = v2 + (frac*(v2-v1)); %interpolation
-    
-    
-    readIndex = readIndex + tableDelta;
-    if readIndex > tableSize
-        readIndex = readIndex-tableSize;
-    end
-    
 end
 
-%soundsc(y, fs);
-figure();
-plot(y);
-
-audioDiscNum = fs*duration/(fs/f0)*N; % number of discontinuities in the audio signal
-discAudio = zeros(1, audioDiscNum); % location of discontinuities in the audio signal expressed in samples
-
-for i=1:audioDiscNum
-    discAudio(i) = fs/(f0*N)*i - fs/f0/np/(2*pi / phaseOffset);
-end
-
-for i=1:audioDiscNum
-    line([discAudio(i),discAudio(i)], [-1,1],'Color','red','LineStyle',':');
-end
-
+soundsc(y, fs);

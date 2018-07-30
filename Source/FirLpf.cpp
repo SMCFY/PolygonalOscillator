df431b4ab0d7dddd0e396827c29a87a389d6f0e3
gericsapo <gericsapo@gmail.com>
2018-07-30 20:14:12 +0200

    lpf class layout

---
+++ Source/FirLpf.cpp
@@ -0,0 +1,30 @@
+
+
+#include "FirLpf.h"
+
+FirLpf::FirLpf(int fs, int numChannels, int samplesPerFame, int filterOrder)
+: filterWindow(dsp::WindowingFunction<float>::blackman)
+{
+    cutoff = new AudioParameterFloat("lpf_fc", "FIR Lowpass cutoff", { 20.f, float(fs/2), 1.0f }, float(fs/2));
+    
+    dsp::ProcessSpec spec = {double(fs), (uint32)samplesPerFrame, (uint32)numChannels};
+    filter.state = dsp::FilterDesign<float>::designFIRLowpassWindowMethod (cutoff->get(), fs, filterOrder, filterWindow); // filter design
+    filter.prepare(spec);
+}
+
+FirLpf::~FirLpf()
+{
+    delete cutoff;
+}
+
+void FirLpf::process(AudioBuffer<float> buff)
+{
+    dsp::AudioBlock<float> block(buff); // init audio block with output buffer
+    filter.process(dsp::ProcessContextReplacing<float>(block)); // process block
+}
+
+void FirLpf::setCutoff(const float& fc)
+{
+    *cutoff = fc;
+}
+
